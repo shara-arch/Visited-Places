6c92cda703f0d50ea22746d277dbc4f77e6edb46
Sharon Moegi <sharamoegi@gmail.com>
2026-02-16 16:17:48 +0300

    Create addDestination function

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ TravelLog.prototype.assignId = function () {
     this.currentId++;
     return this.currentId;
 };
+
+TravelLog.prototype.addDestination = function(location, subLocation, landmark, year, season, month, notes){
+    const newDestination = new this.destinations(location, subLocation, landmark, year, season, month, notes);
+    newDestination.id = this.assignId();
+    this.destinations[newDestination.id] = newDestination;
+
+    alert(`Destination Added Successfully`);
+    this.displayTravelLog();
+};
