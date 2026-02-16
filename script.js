59381059d674c04df75ff706e3b3a2ee53695f35
Sharon Moegi <sharamoegi@gmail.com>
2026-02-16 16:08:54 +0300

    Create assignId function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function TravelLog(){
     this.destinations = {};
     this.currentId = 0;
-}
+};
+
+TravelLog.prototype.assignId = function () {
+    this.currentId++;
+    return this.currentId;
+};
